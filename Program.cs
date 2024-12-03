7b23bacd887e265d5cdf500329b13cd178edf050
Rahmi Kutay Özcan <rahmikutayozcan@gmail.com>
2024-12-04 00:00:00 +0300

    Added new works

--- Program.cs
+++ Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Rahmi Kutay Ozcan");
+            Console.WriteLine("Patika'nin katılımcılarına sagladigi genis kapsamlı egitim programi ile universitede aldigim yazilim muhendisligi bilgilerimi harmanlayip sektore guclu bir giris yapmak.");
+        
+            string input = "42.5";
+            double result = 0;
+
+            double.TryParse(input, out result);
+
+            Console.WriteLine(result);
+        
+        
         }
     }
 }
